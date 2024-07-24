34e3621098970c35db31bb0bf4e37002a06a1dce
jonaspmeise <62212517+jonaspmeise@users.noreply.github.com>
2024-07-24 19:46:24 +0200

    Update!

--- scripts/extract-artwork-data.sh
+++ scripts/extract-artwork-data.sh
@@ -1,14 +1,22 @@
 CURRENT_SHEET_PATH="${CURRENT_SHEET_PATH:=./card-data/beyond.xlsx}"
 TARGET_FOLDER="${TARGET_FOLDER:=./card-data}"
+VOTE_TARGET_FOLDER=${VOTE_TARGET_FOLDER:=./card-data/votes}
 TEMP_FILE_NAME="beyond.txt"
+CARD_SHEET_INDEX=0
+VOTE_SHEET_INDEX=1
 
 if [[ -z "$CURRENT_SHEET_PATH" ]]; then
     echo "Error: Sheet path does not exist!"
     exit 1
 fi
 
+if [ ! -d "$VOTE_TARGET_FOLDER" ]; then
+    mkdir $VOTE_TARGET_FOLDER
+fi
+
 ssconvert --export-file-per-sheet -O "quoting-mode='always'" "./card-data/beyond.xlsx" $TEMP_FILE_NAME
 
+# Export Card-Artworks-URL.
 awk -F, '
     BEGIN {
         # Initialize JSON object
@@ -59,6 +67,33 @@ awk -F, '
         # Close the JSON object
         print "}"
     }
-' $TEMP_FILE_NAME.0 > $TARGET_FOLDER/card-artworks.generated.json
+' $TEMP_FILE_NAME.$CARD_SHEET_INDEX > $TARGET_FOLDER/card-artworks.generated.json
+
+# Export Votes for every User as JSON.
+awk -F, '
+    BEGIN {
+        # Initialize JSON object
+        FS = "\",\""
+    }
+    NR > 1 && NR <= 10 {
+        $0 = substr($0, 2, length($0) - 2)
+        # Extract column B and column A values
+        if($2 != "") {
+            colA = $1
+            colB = $2
+
+            # Prepare the output filename
+            output_filename = colA ".vote.json"
+
+            # Base64 decode column B content
+            "echo " colB " | base64 --decode" | getline decoded_content
+
+            # Write the decoded content to the output file
+            print decoded_content > output_filename
+        }
+    }
+' $TEMP_FILE_NAME.$VOTE_SHEET_INDEX > $TEMP_FILE_NAME.trash
+
+mv *.vote.json "$VOTE_TARGET_FOLDER"
 
 rm $TEMP_FILE_NAME.*
